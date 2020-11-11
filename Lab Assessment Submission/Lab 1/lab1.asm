decb4d7e831fc3847d43cad5a2f1cf664841b8be
Emad Chowdhury <emad.chowdhury@northsouth.com>
2020-11-11 10:33:10 +0600

    lab1

---
+++ Lab Assessment Submission/Lab 1/lab1.asm
@@ -0,0 +1,12 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+org 100h     
+             
+MOV AX,05           
+ADD AX,AX
+ADD AX,1
+             
+ret             
+
+
